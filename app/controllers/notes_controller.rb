8cedf8a0b82d3a6fa8e68077e73549ff737158f1
tprax <tprax01@gmail.com>
2018-08-30 16:56:44 -0600

    updated editor and add home page

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -10,4 +10,35 @@ class NotesController < ApplicationController
   def new
     @note = Note.new
   end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    
+    if @note.update(notes_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+  
+  def create
+    @note = Note.new(notes_params)
+
+    if @note.save
+      redirect_to notes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def notes_params
+      params.require(:note).permit(:title, :date, :note)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_notes#home'
+  get '/about', to: 'static_notes#about'
   resources :notes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
